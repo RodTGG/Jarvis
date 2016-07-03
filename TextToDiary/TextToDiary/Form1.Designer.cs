43b74c361fa533fb3708fecf018788383a2c3ae0
Rod <rodrigosg36@gmail.com>
2016-07-04 06:18:20 +1000

    Main Code
    Jarvis main code, nothing special

---
+++ TextToDiary/TextToDiary/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace TextToDiary
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.ntiTxtDiary = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Enabled = false;
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(93, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 0;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // ntiTxtDiary
+            // 
+            this.ntiTxtDiary.Icon = ((System.Drawing.Icon)(resources.GetObject("ntiTxtDiary.Icon")));
+            this.ntiTxtDiary.Text = "TxtDiary";
+            this.ntiTxtDiary.Visible = true;
+            this.ntiTxtDiary.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.ntiTxtDiary_MouseDoubleClick);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(174, 41);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "TxtDiary";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Main_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.NotifyIcon ntiTxtDiary;
+    }
+}
+
---
+++ TextToDiary/TextToDiary/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Speech.Recognition;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace TextToDiary
+{
+    public partial class Main : Form
+    {
+
+        SpeechRecognitionEngine mySRE = new SpeechRecognitionEngine();
+        string[] lSites;
+        string[] lCommands = { "exit", "new tab", "stop", "jarvis" };
+
+        bool bRunning = true;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            bRunning = false;
+            Disable();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            bRunning = true;
+            Enable();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            WindowSetup();
+            LoadFile();
+
+            Choices Commands = new Choices(lCommands);
+            Grammar gm = new Grammar(Commands);
+
+            mySRE.RequestRecognizerUpdate();
+            mySRE.SetInputToDefaultAudioDevice();
+            mySRE.LoadGrammar(gm);
+            mySRE.SpeechRecognized += mySR_SpeechRecognized;
+            mySRE.RecognizeAsync(RecognizeMode.Multiple);
+
+        }
+
+        void mySR_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            if (e.Result.Text == "jarvis")
+            {
+                bRunning = true;
+                Enable();
+            }
+
+            if (bRunning)
+            {
+                try
+                {
+                    switch (e.Result.Text)
+                    {
+                        case "exit":
+                            Application.Exit();
+                            break;
+                        case "new tab":
+                            Process.Start("chrome", "https://www.google.com.au/");
+                            break;
+                        case "stop":
+                            bRunning = false;
+                            Disable();
+                            break;
+                    }
+
+                    if (e.Result.Text == "plex")
+                    {
+                        Process.Start("chrome", "http://192.168.1.201:32400/web/index.html");
+
+                    }
+                    else if (lSites.Contains(e.Result.Text))
+                    {
+                        Process.Start("chrome", "http://www." + e.Result.Text + ".com");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.ToString());
+                }
+            }
+        }
+
+        void WindowSetup()
+        {
+            Rectangle workingArea = Screen.GetWorkingArea(this);
+            this.Location = new Point(workingArea.Right - Size.Width,
+                                      workingArea.Bottom - Size.Height);
+        }
+
+        private void Main_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == WindowState)
+            {
+                Hide();
+            }
+        }
+
+        private void ntiTxtDiary_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.BringToFront();
+        }
+
+        private void Enable()
+        {
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+        }
+
+        private void Disable()
+        {
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+        }
+
+        private void LoadFile()
+        {
+            DialogResult msgBoxResult;
+            string path = "websites.txt";
+
+            if (!File.Exists(path))
+            {
+                msgBoxResult = MessageBox.Show("Error websites.txt not found\n Create default websites?", "Error in IO", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                if (msgBoxResult == DialogResult.Cancel)
+                {
+                    MessageBox.Show("Closing", "Closing");
+                    Application.Exit();
+                }
+                else
+                {
+                    using (StreamWriter sw = File.CreateText(path))
+                    {
+                        string lSites = "facebook\ntwitch\nyoutube\nplex\nnetflix\nreddit";
+                        sw.Write(lSites);
+                    }
+                }
+            }
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string sLine = "";
+                List<string> temp = new List<string>();
+
+                for (int i = 0; (sLine = sr.ReadLine()) != null; i++)
+                {
+                    temp.Add(sLine);
+                }
+
+                lSites = new string[temp.Count];
+
+                for (int i = 0; i < temp.Count; i++)
+                {
+                    lSites[i] = temp[i];
+                }
+            }
+
+            SetupCommand();
+        }
+
+        void SetupCommand()
+        {
+            List<string> tempCommand = new List<string>();
+
+
+            for (int i = 0; i < lCommands.Length; i++)
+            {
+                tempCommand.Add(lCommands[i]);
+            }
+
+            for (int i = 0; i < lSites.Length; i++)
+            {
+                tempCommand.Add(lSites[i]);
+            }
+
+            lCommands = new string[lCommands.Length + lSites.Length];
+            for (int i = 0; i < tempCommand.Count; i++)
+            {
+                lCommands[i] = tempCommand[i];
+            }
+        }
+    }
+}
